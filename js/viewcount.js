c7fcb59c1f18f80328fe5296d7340ff591c148cb
Hoyle95 <140357015+Hoyle95@users.noreply.github.com>
2025-02-21 10:16:29 +0000

    basic landing page with player

---
+++ js/viewcount.js
@@ -0,0 +1,14 @@
+const endpoint = "https://ac.tetrahydro.radio/api/station/TetraHydroRadio";
+
+function fetchUniqueViewers() {
+  fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {
+      const uniqueViewers = data.mounts[0].listeners.unique;
+      document.getElementById("unique-viewers").textContent = uniqueViewers;
+    })
+    .catch(error => console.error("Error fetching data:", error));
+}
+
+fetchUniqueViewers()
+setInterval(fetchUniqueViewers, 10000);
